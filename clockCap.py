7c06ace8abafe96872b2d6e5c9ab6930f1b280fc
a4a881d4 <a4a881d4@163.com>
2015-10-12 23:18:08 +0800

    pre log

--- clockCap.py
+++ clockCap.py
@@ -21,11 +21,14 @@ class clockCap:
 		return sys,(cpu0+cpu1)/2		
 	
 	def log(self):
-		while len(self.mem)>self.N:
+		while len(self.mem)>=self.N:
 			del self.mem[0]
 		self.mem.append(self.timePair())
 		
 	def run(self):
+		while len(self.mem)<self.N:
+			#time.sleep(0.0001)
+			self.log()
 		while self.Stop==0:
 			time.sleep(0.1)
 			self.log()
@@ -47,6 +50,11 @@ class clockCap:
 		self.offset = long(f[0,0]*(1<<48))
 		self.cpu0 = x0
 		self.sys0 = y0
+		self.ys = ys
+		self.xs = xs
+		self.xxs = xxs
+		self.yxs = yxs
+		self.A = A
 	
 	def Now(self):
 		cpu = self.lib.getNow()
@@ -62,13 +70,14 @@ def main():
 	t = threading.Thread(target=clk.run)
 	t.start()
 	for i in range(600):
+		time.sleep(1)
 		print clk.factor,
 		cpu0 = clk.Now()		
 		now = time.time()*1e9
 		cpu1 = clk.Now()
 		print now-(cpu0+cpu1)/2
 		clk.calc()
-		time.sleep(1)
+		#print clk.A,clk.xs,clk.ys,clk.xxs,clk.yxs,len(clk.mem)
 	clk.Stop = 1
 	time.sleep(1)
 	print "Exit"
